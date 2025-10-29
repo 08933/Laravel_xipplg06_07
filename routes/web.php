1301ab0cd3e40972447ca0bf28f34242fd9ab8b4
Chelsya Lovelyna <chelynnachei@gmail.com>
2025-10-29 15:19:28 +0700

    Add Migration Student

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
 
@@ -11,20 +10,10 @@ Route::get('/', [LandingController::class, 'index'])->name('landing');
 // Route untuk halaman dashboard admin
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-=======
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Default route Laravel (bisa dipakai kalau mau)
 Route::get('/', function () {
     return view('welcome');
->>>>>>> c8cb8b3d2489cc5ddad50e6e3299c622e5bd5a4c
 });
+
